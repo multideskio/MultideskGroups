db509a7992f57b43c2a340808b1a597e34790e48
Paulo Henrique Duarte <multidesk.io@gmail.com>
2024-10-22 09:26:51 -0300

    Update whatsapp_helper.php

--- app/Helpers/whatsapp_helper.php
+++ app/Helpers/whatsapp_helper.php
@@ -1,123 +1,94 @@
-<?php // app/Helpers/WhatsAppHelper.php
+<?php
+
+// app/Helpers/WhatsAppHelper.php
 
 if (!function_exists('createTextMessage')) {
-    function createTextMessage($number, $message, bool $mentions = false) {
-        return array(
-            "number" => $number,
-            "options" => array(
-                "delay" => 1200,
-                "presence" => "composing",
-                "mentions" => array(
-                    "everyOne" => $mentions
-                )
-            ),
-            "textMessage" => array(
-                "text" => $message
-            )
-        );
+    function createTextMessage($number, $message, bool $mentions = false)
+    {
+        return [
+            "number"           => $number,
+            "text"             => $message,
+            "mentionsEveryOne" => $mentions,
+        ];
     }
 }
 
 if (!function_exists('createImageMessage')) {
-    function createImageMessage($number, $caption, $mediaUrl) {
-        return array(
-            "number" => $number,
-            "options" => array(
-                "delay" => 1200,
-                "presence" => "composing"
-            ),
-            "mediaMessage" => array(
-                "mediatype" => "image",
-                "caption" => $caption,
-                "media" => $mediaUrl
-            )
-        );
+    function createImageMessage($number, $caption, $mediaUrl, bool $mentions = false)
+    {
+        return [
+            "number"           => $number,
+            "mediatype"        => "image",
+            "caption"          => $caption,
+            "media"            => $mediaUrl,
+            "mentionsEveryOne" => $mentions,
+        ];
     }
 }
 
 if (!function_exists('createVideoMessage')) {
-    function createVideoMessage($number, $caption, $mediaUrl) {
-        return array(
-            "number" => $number,
-            "options" => array(
-                "delay" => 1200,
-                "presence" => "composing"
-            ),
-            "mediaMessage" => array(
-                "mediatype" => "video",
-                "caption" => $caption,
-                "media" => $mediaUrl
-            )
-        );
+    function createVideoMessage($number, $caption, $mediaUrl, bool $mentions = false)
+    {
+        return [
+            "number"           => $number,
+            "mediatype"        => "video",
+            "caption"          => $caption,
+            "media"            => $mediaUrl,
+            "mentionsEveryOne" => $mentions,
+        ];
     }
 }
 
 if (!function_exists('createPdfDocumentMessage')) {
-    function createPdfDocumentMessage($number, $fileName, $caption, $mediaUrl) {
-        return array(
-            "number" => $number,
-            "options" => array(
-                "delay" => 1200,
-                "presence" => "composing"
-            ),
-            "mediaMessage" => array(
-                "mediatype" => "document",
-                "fileName" => $fileName,
-                "caption" => $caption,
-                "media" => $mediaUrl
-            )
-        );
+    function createPdfDocumentMessage($number, $fileName, $caption, $mediaUrl, bool $mentions = false)
+    {
+        return [
+            "number"           => $number,
+            "mediatype"        => "document",
+            "fileName"         => $fileName,
+            "caption"          => $caption,
+            "media"            => $mediaUrl,
+            "mentionsEveryOne" => $mentions,
+        ];
     }
 }
 
 if (!function_exists('createXlsxDocumentMessage')) {
-    function createXlsxDocumentMessage($number, $fileName, $caption, $mediaUrl) {
-        return array(
-            "number" => $number,
-            "options" => array(
-                "delay" => 1200,
-                "presence" => "composing"
-            ),
-            "mediaMessage" => array(
-                "mediatype" => "document",
-                "fileName" => $fileName,
-                "caption" => $caption,
-                "media" => $mediaUrl
-            )
-        );
+    function createXlsxDocumentMessage($number, $fileName, $caption, $mediaUrl, bool $mentions = false)
+    {
+        return [
+            "number"           => $number,
+            "mediatype"        => "document",
+            "fileName"         => $fileName,
+            "caption"          => $caption,
+            "media"            => $mediaUrl,
+            "mentionsEveryOne" => $mentions,
+
+        ];
     }
 }
 
 if (!function_exists('createZipDocumentMessage')) {
-    function createZipDocumentMessage($number, $fileName, $caption, $mediaUrl) {
-        return array(
-            "number" => $number,
-            "options" => array(
-                "delay" => 1200,
-                "presence" => "composing"
-            ),
-            "mediaMessage" => array(
-                "mediatype" => "document",
-                "fileName" => $fileName,
-                "caption" => $caption,
-                "media" => $mediaUrl
-            )
-        );
+    function createZipDocumentMessage($number, $fileName, $caption, $mediaUrl, bool $mentions = false)
+    {
+        return [
+            "number"           => $number,
+            "mediatype"        => "document",
+            "fileName"         => $fileName,
+            "caption"          => $caption,
+            "media"            => $mediaUrl,
+            "mentionsEveryOne" => $mentions,
+        ];
     }
 }
 
 if (!function_exists('createAudioMessage')) {
-    function createAudioMessage($number, $audioUrl) {
-        return array(
-            "number" => $number,
-            "options" => array(
-                "delay" => 1200,
-                "presence" => "recording",
-                "encoding" => true
-            ),
-            "audioMessage" => array(
-                "audio" => $audioUrl
-            )
-        );
+    function createAudioMessage($number, $audioUrl, bool $mentions = false)
+    {
+        return [
+            "number"           => $number,
+            "audio"            => $audioUrl,
+            "mentionsEveryOne" => $mentions,
+        ];
     }
 }
